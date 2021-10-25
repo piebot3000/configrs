d68e51e3976385a0b13519a1f2ba9637b3521a55
sage wynn <sage@schpeem.ec.rr.com>
2021-10-24 22:09:19 -0400

    changed over to structopt instead of clap

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
-use clap::{App, AppSettings, Arg, SubCommand};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::path::PathBuf;
+use structopt::StructOpt;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
@@ -18,86 +18,30 @@ impl std::default::Default for Config {
     }
 }
 
-pub fn build() -> App<'static, 'static> {
-    App::new("configrs")
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .version("0.1.0")
-        .author("Sage Wynn")
-        .about("Helps keep track of config files.")
-        .arg(
-            Arg::with_name("config")
-                .short("c")
-                .long("config")
-                .help("sets a custom config file")
-                .takes_value(true),
-        )
-        .subcommand(
-            SubCommand::with_name("edit")
-                .about("edits a file in the repository")
-                .arg(
-                    Arg::with_name("file")
-                        .help("which file to be edited")
-                        .required(true)
-                        .index(1),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("add")
-                .about("adds a new file to the repository")
-                .arg(
-                    Arg::with_name("name")
-                        .help("name for the file")
-                        .required(true)
-                        .index(1),
-                )
-                .arg(
-                    Arg::with_name("file")
-                        .help("path of the config file")
-                        .required(true)
-                        .index(2),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("remove")
-                .about("removes a file from the repository")
-                .arg(
-                    Arg::with_name("file")
-                        .help("file to be removed")
-                        .required(true)
-                        .index(1),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("yoink")
-                .about(
-                    "grab all files from their place on the system and copy them to the directory",
-                )
-                .arg(
-                    Arg::with_name("directory")
-                        .help("directory to store the files")
-                        .required(true)
-                        .index(1),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("yeet")
-                .about("restore all files in the config dir to their place on the system")
-                .arg(
-                    Arg::with_name("directory")
-                        .help("directory to store the files")
-                        .required(true)
-                        .index(1),
-                )
-                .arg(
-                    Arg::with_name("dry_run")
-                        .help("perform a dry run and do no actions")
-                        .short("d")
-                        .long("dry_run"),
-                ),
-        )
+#[derive(StructOpt)]
+#[structopt(name="Configrs", about="simple utility to help deal with config files")]
+pub struct Opt {
+    #[structopt(short, long)]
+    pub add: bool,
+    #[structopt(short, long)]
+    pub remove: bool,
+    #[structopt(short, long)]
+    pub yoink: bool,
+
+    #[structopt()]
+    pub name: String,
+
+    #[structopt(short, long="config")]
+    pub config_file: Option<PathBuf>,
+
+    #[structopt(required_if("add", "true"), required_if("remove", "true"))]
+    pub file: Option<PathBuf>,
+
+    #[structopt(required_if("yoink", "true"))]
+    pub directory: Option<PathBuf>,
 }
 
-pub fn load_cfg(path: Option<&str>) -> Result<Config, confy::ConfyError> {
+pub fn load_cfg(path: &Option<PathBuf>) -> Result<Config, confy::ConfyError> {
     // loading either from confys default, or a user supplied config
     match path {
         Some(dir) => confy::load_path(&dir),
@@ -105,7 +49,7 @@ pub fn load_cfg(path: Option<&str>) -> Result<Config, confy::ConfyError> {
     }
 }
 
-pub fn store_cfg(path: Option<&str>, cfg: Config) -> Result<(), confy::ConfyError> {
+pub fn store_cfg(path: &Option<PathBuf>, cfg: Config) -> Result<(), confy::ConfyError> {
     // saving to either confys default or a user supplied default
     match path {
         Some(dir) => confy::store_path(&dir, cfg),
--- src/main.rs
+++ src/main.rs
@@ -3,103 +3,47 @@ use anyhow::bail;
 use anyhow::Result;
 use std::fs;
 use std::path::PathBuf;
+use structopt::StructOpt;
 
 mod app;
 fn run() -> Result<()> {
     // setting up arguments and whatnot
-    let args = app::build().get_matches();
+    let args = app::Opt::from_args();
 
     // loading either from confys default, or a user supplied config
-    let mut cfg: app::Config = app::load_cfg(args.value_of("config"))?;
+    let mut cfg: app::Config = app::load_cfg(&args.config_file)?;
 
     // check to see if we need to rewrite the config before exiting
     let mut changed_config: bool = false;
 
     // match which subcommand has been used
-    match args.subcommand() {
-        ("edit", Some(edit_args)) => {
-            let file = edit_args
-                .value_of("file")
-                .expect("file somehow missing, clap.");
-
-            edit(&mut cfg, file)?;
-        }
-
-        ("add", Some(add_args)) => {
-            let name = add_args
-                .value_of("name")
-                .expect("name somehow missing, clap.");
-            let file = add_args
-                .value_of("file")
-                .expect("file somehow missing, clap.");
-
-            add(&mut cfg, name, file);
-            changed_config = true;
-        }
-
-        ("remove", Some(remove_args)) => {
-            let file = remove_args
-                .value_of("file")
-                .expect("name somehow missing, clap.");
-
-            remove(&mut cfg, file)?;
-            changed_config = true;
-        }
-
-        ("yoink", Some(yoink_args)) => {
-            let directory = PathBuf::from(
-                yoink_args
-                    .value_of("directory")
-                    .expect("directory somehow missing, clap."),
-            );
-
-            yoink(&cfg, directory)?;
-        }
-
-        ("yeet", Some(yeet_args)) => {
-            let dry_run = yeet_args.is_present("dry_run");
-            let directory = PathBuf::from(
-                yeet_args
-                    .value_of("directory")
-                    .expect("directory somehow missing, clap."),
-            );
-
-            yeet(&cfg, dry_run, directory)?;
-        }
-        _ => unreachable!(),
+    if args.add {
+        add(&mut cfg, &args.name, &args.file.unwrap());
+        changed_config = true;
+    } else if args.remove {
+        remove(&mut cfg, &args.name)?;
+        changed_config = true;
+    } else if args.yoink {
+        yoink(&cfg, &args.directory.unwrap())?;
+    } else {
+        edit(&mut cfg, &args.name)?;
     }
 
     // if we changed the config (add, remove) then we need to store it again
     if changed_config {
-        app::store_cfg(args.value_of("config"), cfg)?;
+        app::store_cfg(&args.config_file, cfg)?;
     }
 
     // everything went ok
     Ok(())
 }
 
-fn yoink(cfg: &app::Config, directory: PathBuf) -> Result<()> {
+fn yoink(cfg: &app::Config, directory: &PathBuf) -> Result<()> {
     for (name, file) in &cfg.repo {
         let mut dir = directory.clone();
         dir.push(&name);
         println!("Moving {:?} to {:?}", &file, &dir);
         fs::copy(file, dir)?;
-    }
-
-    Ok(())
-}
-
-fn yeet(cfg: &app::Config, dry_run: bool, directory: PathBuf) -> Result<()> {
-    if dry_run {
-        println!("Doing a dry run");
-    }
-    for (name, file) in &cfg.repo {
-        let mut dir = directory.clone();
-        dir.push(&name);
-        println!("Moving file {:?} to {:?}", &dir, &file);
-        if !dry_run {
-            fs::copy(dir, file)?;
-        }
     }
 
     Ok(())
@@ -116,9 +60,9 @@ fn remove(cfg: &mut app::Config, name: &str) -> Result<()> {
     }
 }
 
-fn add(cfg: &mut app::Config, name: &str, file: &str) {
+fn add(cfg: &mut app::Config, name: &str, file: &PathBuf) {
     cfg.repo.insert(name.to_string(), PathBuf::from(file));
-    println!("Added {} => {} to the repository", name, file);
+    println!("Added {} => {:?} to the repository", name, file);
 }
 
 fn edit(cfg: &mut app::Config, name: &str) -> Result<()> {
